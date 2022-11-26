3a5d362d68b4ee3da764c12799c70266aadafa38
Sam Richter <hoodie888@outlook.com>
2022-11-26 17:16:24 -0600

    libs volume

--- container.sh
+++ container.sh
@@ -14,7 +14,7 @@ CONTAINER_NAME=${CONTAINER_NAME:-test_deb}
 
 ${DOCKER} build -t test-deb "${DIR}"
 #${DOCKER} pull wpilib/pi-gen:latest
-${DOCKER} run --privileged --cap-add=MKNOD test-deb bash -e -o pipefail -c "cd /test-tmp; ./run.sh"
+${DOCKER} run --privileged --cap-add=MKNOD test-deb --volume /usr/lib:/test-tmp/lib-deps:ro bash -e -o pipefail -c "cd /test-tmp; ./run.sh"
 
 # apt update
 # apt install -y wget xz-utils bzip2 make autoconf gcc-multilib g++-multilib

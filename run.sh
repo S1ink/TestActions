b3015a749701de6af83137c6eb325a20aa0af0b6
Sam Richter <hoodie888@outlook.com>
2022-11-26 17:35:33 -0600

    ldconfig

--- run.sh
+++ run.sh
@@ -8,4 +8,9 @@ tar xzf ./armhf-raspi-bullseye-2023-i686-linux-gnu-Toolchain-10.2.0.tgz
 export PATH=/test-tmp/raspi-bullseye/bin:${PATH}
 export PATH=/test-tmp/lib-deps:${PATH}
 
+sudo touch /etc/ld.so.conf.d/cc-10.conf
+echo "/test-tmp/lib-deps" | sudo tee -a /etc/ld.so.conf.d/cc-10.conf
+sudo ldconfig -v | less
+
 echo $(armv6-bullseye-linux-gnueabihf-g++ --version)
+echo $(LD_LIBRARY_PATH=/test-tmp/lib-deps armv6-bullseye-linux-gnueabihf-g++ --version)

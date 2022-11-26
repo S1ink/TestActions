5ad2f03b558170a1410f2427768d23d1c02a49d9
Sam Richter <hoodie888@outlook.com>
2022-11-26 17:17:22 -0600

    deps -> path

--- run.sh
+++ run.sh
@@ -6,5 +6,6 @@ sudo apt-get install wget
 wget -nc -nv https://github.com/wpilibsuite/opensdk/releases/download/v2023-4/armhf-raspi-bullseye-2023-i686-linux-gnu-Toolchain-10.2.0.tgz
 tar xzf ./armhf-raspi-bullseye-2023-i686-linux-gnu-Toolchain-10.2.0.tgz
 export PATH=/test-tmp/raspi-bullseye/bin:${PATH}
+export PATH=/test-tmp/lib-deps:${PATH}
 
 echo $(armv6-bullseye-linux-gnueabihf-g++ --version)

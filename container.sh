546724d69bda5c18f6e5aa40e7e2e731d9dbbdf5
Sam Richter <hoodie888@outlook.com>
2022-11-26 18:42:22 -0600

    lib32

--- container.sh
+++ container.sh
@@ -16,7 +16,7 @@ ${DOCKER} build -t test-deb "${DIR}"
 #${DOCKER} pull wpilib/pi-gen:latest
 ${DOCKER} run \
 	--privileged --cap-add=MKNOD \
-	--volume /lib/x86_64-linux-gnu:/test-tmp/lib-deps:ro \
+	--volume /lib32:/test-tmp/lib-deps:ro \
 	test-deb \
 	bash -e -o pipefail -c "cd /test-tmp; ./run.sh"
 #	bash -e -o pipefail -c "cd /test-tmp/lib-deps; ls -l && cd /test-tmp; ./run.sh"
